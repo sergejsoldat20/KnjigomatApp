df1fc2a631b192eaedfff07db711221da4a7b410
Sergej Soldat <sergej.s.soldat20@gmail.com>
2023-03-12 13:41:39 +0100

    cloudinary first commit

---
+++ KnjigomatBackend/src/main/java/web/books/controllers/PhotoController.java
@@ -0,0 +1,9 @@
+package web.books.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("photos")
+public class PhotoController {
+}
